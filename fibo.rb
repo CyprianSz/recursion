e269c61b34f7ce29de901e4086335a63b0ce429d
CyprianS <szwajka.cyprian@gmail.com>
2017-02-20 22:32:48 +0100

    tabs

--- fibo.rb
+++ fibo.rb
@@ -1,25 +1,25 @@
 def fibs(n)
-	array = [1, 2]
-	if n == 1
-		array = [1]
-	else 
-		x = 0
-		until array.length == n
-			array << array[x + 0] + array[x + 1]
-			x += 1
-		end
-	end
-	array
+  array = [1, 2]
+  if n == 1
+    array = [1]
+  else 
+    x = 0
+    until array.length == n
+      array << array[x + 0] + array[x + 1]
+      x += 1
+    end
+  end
+  array
 end
 
 def fibs_rec(n, array = [1,2])
-	if n == 1 
-		[1]
-	elsif n == 2
-		array
-	else
-		fibs_rec(n - 1, array << array[-2] + array [-1])
-	end
+  if n == 1 
+    [1]
+  elsif n == 2
+    array
+  else
+    fibs_rec(n - 1, array << array[-2] + array [-1])
+  end
 
 end
 
